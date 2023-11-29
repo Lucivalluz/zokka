96709d60297fec8de5fc2bcedaf40be29da1875d
lucivalluz <lucivalsouza@hotmail.com>
2023-11-28 21:40:10 -0300

    Zokka

---
+++ zokka.js
@@ -0,0 +1,15 @@
+const element = document.querySelector(".his")
+
+function cliqueiNoBotãoHis() {
+    element.innerHTML = "Somos uma empresa especializada em serviços de usinagem, bem como fabricação de peças em metais ferrosos e não ferrosos. Nacionalizamos peças com perfis complexos, projetos mecânicos, eixos, buchas, engrenagens ou rolos com ou sem tratamento térmico, e revestimento de borracha. Ainda dispomos de programas informatizados para a geração de cadastro dos desenhos das peças fabricadas, para os mais variados setores da indústria."
+
+    
+}
+
+const elements = document.querySelector(".serv")
+
+function cliqueiNoBotãoServ() {
+
+    elements.image = ("assets/Bico_gás.jpg")
+
+}
